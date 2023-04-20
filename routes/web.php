c1a3701bc9f45097510dd91137d9332d1e05173f
yugomoro <yugo.moro@tokyo.supersoftware.co.jp>
2023-04-20 05:25:49 +0000

    C7 ルート定義のスケルトンを作成

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
+use App\Book;
+use Illuminate\Http\Request;
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+/**
+* 本の一覧表示(books.blade.php)
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+* 本を追加 
+*/
+Route::post('/books', function (Request $request) {
+    //
+});
+
+/**
+* 本を削除 
+*/
+Route::delete('/book/{book}', function (Book $book) {
+    //
 });

8773cd626f5ef4e49e576c80bac4f042e6e6d1f0
yugomoro <yugo.moro@tokyo.supersoftware.co.jp>
2023-04-21 01:54:47 +0000

    C12 本データの削除機能作成、入力フォーム・バリデーション・テストデータの追加、更新画面・更新処理の追加

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,40 +1,56 @@
 <!-- resources/views/books.blade.php -->
 @extends('layouts.app')
 @section('content')
-
     <!-- Bootstrapの定形コード… -->
     <div class="card-body">
         <div class="card-title">
             本のタイトル
         </div>
-        
+
         <!-- バリデーションエラーの表示に使用-->
         @include('common.errors')
         <!-- バリデーションエラーの表示に使用-->
 
-        <!-- 本登録フォーム -->
+        <!-- 本のタイトル -->
         <form action="{{ url('books') }}" method="POST" class="form-horizontal">
             @csrf
-
-            <!-- 本のタイトル -->
-            <div class="form-group">
-                <div class="col-sm-6">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="book" class="col-sm-3 control-label">Book</label>
                     <input type="text" name="item_name" class="form-control">
+                </div>
+
+                <div class="form-group col-md-6">
+                    <label for="amount" class="col-sm-3 control-label">金額</label>
+                    <input type="text" name="item_amount" class="form-control">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="number" class="col-sm-3 control-label">数</label>
+                    <input type="text" name="item_number" class="form-control">
+                </div>
+
+                  <div class="form-group col-md-6">
+                    <label for="published" class="col-sm-3 control-label">公開日</label>
+                    <input type="date" name="published" class="form-control">
                 </div>
             </div>
 
             <!-- 本 登録ボタン -->
-            <div class="form-group">
+            <div class="form-row">
                 <div class="col-sm-offset-3 col-sm-6">
                     <button type="submit" class="btn btn-primary">
-                        Save
+                    Save
                     </button>
                 </div>
             </div>
         </form>
     </div>
+
+
     <!-- Book: 既に登録されてる本のリスト -->
-    <!-- 現在の本 -->
+     <!-- 現在の本 -->
     @if (count($books) > 0)
         <div class="card-body">
             <div class="card-body">
@@ -53,10 +69,28 @@
                                     <div>{{ $book->item_name }}</div>
                                 </td>
 
+                                <!-- 本: 更新ボタン -->
+                                <td>
+                                    <form action="{{ url('booksedit/'.$book->id) }}" method="POST">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">
+                                            更新
+                                        </button>
+                                    </form>
+                                </td>
+
                                 <!-- 本: 削除ボタン -->
                                 <td>
+                                    <form action="{{ url('book/'.$book->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
 
+                                        <button type="submit" class="btn btn-danger">
+                                            削除
+                                        </button>
+                                    </form>
                                 </td>
+
                             </tr>
                         @endforeach
                     </tbody>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 use App\Book;
 use Illuminate\Http\Request;
+
 
 /**
 * 本の一覧表示(books.blade.php)
@@ -26,45 +16,73 @@ Route::get('/', function () {
 });
 
 /**
-* 本を追加 
+* 本を登録
 */
 Route::post('/books', function (Request $request) {
-
     //バリデーション
     $validator = Validator::make($request->all(), [
-        'item_name' => 'required|max:255',
+        'item_name' => 'required|min:3|max:255',
+        'item_number' => 'required | min:1 | max:3',
+        'item_amount' => 'required | max:6',
+         'published'   => 'required',
     ]);
-
     //バリデーション:エラー 
     if ($validator->fails()) {
         return redirect('/')
             ->withInput()
             ->withErrors($validator);
     }
-    
-    //以下登録処理が後で追加します！
     // Eloquentモデル（登録処理）
     $books = new Book;
-    $books->item_name = $request->item_name;
-    $books->item_number = '1';
-    $books->item_amount = '1000';
-    $books->published = '2017-03-07 00:00:00';
+    $books->item_name =    $request->item_name;
+    $books->item_number =  $request->item_number;
+    $books->item_amount =  $request->item_amount;
+    $books->published =    $request->published;
     $books->save(); 
     return redirect('/');
 
-
 });
 
+//更新画面
+Route::post('/booksedit/{books}', function(Book $books) {
+    //{books}id 値を取得 => Book $books id 値の1レコード取得
+    return view('booksedit', ['book' => $books]);
+});
+
+//更新処理
+Route::post('/books/update', function(Request $request){
+    //バリデーション
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'item_name' => 'required|min:3|max:255',
+            'item_number' => 'required|min:1|max:3',
+            'item_amount' => 'required|max:6',
+            'published' => 'required',
+    ]);
+    //バリデーション:エラー
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+    }
+    
+    //データ更新
+    $books = Book::find($request->id);
+    $books->item_name   = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published   = $request->published;
+    $books->save();
+    return redirect('/');
+});
 
 /**
 * 本を削除 
 */
 Route::delete('/book/{book}', function (Book $book) {
-    //
+    $book->delete();
+    return redirect('/');
 });
 
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
